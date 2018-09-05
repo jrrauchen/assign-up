d1ad44d0ee6fea9e908f15e22ebc690df3d213e0
DESKTOP-33R4HN0\jrrau <jrrauchen@gmail.com>
2018-09-05 12:38:26 -0400

    done?

--- routes/db-routes.js
+++ routes/db-routes.js
@@ -8,24 +8,48 @@ router.get('/', function(req,res,next) {
   res.render()
 });
 
+//get assign for a student
+// router.get('/getAssignment', function(req,res,next) {
+//   var result = [];
+//   mongo.connect(url, function(err, db){
+//     assert.equal(null, err);
+//     var cursor = db.collection('assignments').find();
+//     cursor.forEach(function(data, err){
+//       assert.(null, err);
+//       results.push(data);
+//     }, function(data, err){
+//       db.close();
+//       res.render('index', {items: result});
+//     });
+//   });
+// });
 router.get('/getAssignment', function(req,res,next) {
   mongo.connect(url, function(err, db){
     assert.equal(null, err);
-    var cursor = db.collection('assignments').find();
-    cursor.forEach();
-    var cursor;
+    var cursor = db.collection('assignments').find({"UserID":req.user.googleId});
+    cursor.forEach(function(data, err){
+      assert.equal(null, err);
+      var item = {
+        Course : req.body.course,
+        AssignmentTitle: req.body.title
+      };
+      console.log(item);
+    }, function(data, err){
+      db.close();
+      //res.render('index', {items: });
+    });
   });
 });
 
 router.post('/insert', function(req,res,next) {
   var item = {
     UserID: req.user.googleId,
-    Course: req.body.Course,
-    AssignmentTitle: req.body.AssignmentTitle,
-    AssignmentDesc: req.body.AssignmentTitle,
-    CreatedDate: req.body.CreatedDate,
-    DueDate: req.body.DueDate,
-    Complete: req.body.Complete
+    Course: req.body.course,
+    AssignmentTitle: req.body.title,
+    AssignmentDesc: req.body.details,
+    CreatedDate: req.body.createdDate,
+    DueDate: req.body.dueDate,
+    Complete: req.body.completed
   };
   mongo.connect(url, function(err, db){
     assert.equal(null, err);
@@ -38,7 +62,7 @@ router.post('/insert', function(req,res,next) {
 });
 
 router.post('/delete', function(req,res,next) {
-  var assignment = req.body.AssignmentTitle;
+  var assignment = req.body.title;
   var id = req.user.googleId;
 
   mongo.connect(url, function(err, db){
--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-
+const dbRoutes = require('./routes/db-routes');
+const mongo = require('mongodb');
+var assert = require('assert');
 const app = express();
+let bodyParser = require('body-parser')
 
 app.set('view engine','ejs');
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+
+
 
 app.use(cookieSession({
   maxAge: 24*60*60*1000,
@@ -24,17 +31,70 @@ app.use(passport.session());
 mongoose.connect(keys.mongodb.dbURI, () =>{
   console.log('connected to mongodb');
 });
+
 //set up routes
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
+
 
 app.get('/',(req,res) => {
   res.render('home', {user: req.user });
 });
 
-app.get('/tt', (req, res, next) => {
-  console.log(req.user.googleId);
+app.get('/getAssignment', function(req,res,next) {
+  mongo.connect(keys.mongodb.dbURI, function(err, db){
+    console.log(db.collection('assignments'))
+    var cursor = db.collection('assignments').find({"UserID":req.user.googleId});
+    var responseData = {};
+    cursor.forEach(function(data, err){
+      if(Object.keys(responseData).includes(data.Course)){
+        responseData[data.Course].push(data)
+      }else{
+        responseData[data.Course] = [];
+        responseData[data.Course].push(data);
+      }
+    }, function(data, err){
+      db.close();
+      res.send(responseData);
+    });
+  });
 });
+
+app.post('/insert', function(req,res,next) {
+  console.log(req)
+  var item = {
+    UserID: req.user.googleId,
+    Course: req.body.course,
+    AssignmentTitle: req.body.title,
+    AssignmentDesc: req.body.details,
+    CreatedDate: req.body.createdDate,
+    DueDate: req.body.dueDate,
+    Complete: req.body.completed
+  };
+  mongo.connect(keys.mongodb.dbURI, function(err, db){
+    db.collection('assignments').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('assignment inserted');
+      db.close();
+      res.send()
+    });
+  });
+});
+
+app.post('/delete', function(req,res,next) {
+  var assignment = req.body.title;
+  var id = req.user.googleId;
+
+  mongo.connect(keys.mongodb.dbURI, function(err, db){
+    assert.equal(null, err);
+    db.collection('assignments').deleteOne({"AssignmentTitle": assignment,"UserID": id }, function(err, result) {
+      assert.equal(null, err);
+      console.log('assignment deleted');
+      db.close();
+    });
+  });
+});
+
 
 
 app.listen(8080, '0.0.0.0',() => {
